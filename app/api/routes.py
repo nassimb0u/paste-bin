e8f3199d373850d76de3a5449dbbdb18ac248493
Nassim Boulechfar <in_boulechfar@esi.dz>
2021-03-19 20:15:18 +0100

    feat: error handling for poste /pastes/

--- app/api/routes.py
+++ app/api/routes.py
@@ -20,38 +20,84 @@ def generate_id(timestamp):
         i += 2
     shuffle(bytes_list)
     return ''.join(bytes_list)
-    
-    
 
-
-@name_space.route("/")
+@name_space.route('/')
 class PasteList(Resource):
     def post(self):
         body = request.get_json()
         print(type(body))
         if body is None:
-            name_space.abort(400)
+            name_space.abort(400, **handle_bad_request_400)
         f = 1000
         if ID_CONFIG['time_unit'] == 'us':
             f *= 1000
         timestamp = round(time()*f)
         paste_id = generate_id(timestamp)
-        paste = Paste(paste_id, body.get('title'), body.get('body'))
+        try:
+            title = body['title']
+            body = body['body']
+        except KeyError as e:
+            raise InvalidRequestError('missing required argument', e.args[0])
+        if type(title) != str:
+            raise InvalidRequestError(
+                'wrong parameter type, string expected',
+                'title')
+        if type(body) != str:
+            raise InvalidRequestError(
+                'wrong parameter type, string expected',
+                'body')
+        paste = Paste(paste_id, title, body)
         paste.insert()
         return {
-            "success": True,
-            "created": paste_id
+            'success': True,
+            'created': paste_id
         }
 
 
-@name_space.route("/<string:id>")
+@name_space.route('/<string:id>')
 class PasteDetail(Resource):
     def get(self, id):
         paste = Paste.query.get(id)
         if paste is None:
-            name_space.abort(404)
+            name_space.abort(404, **handle_not_found_404)
         schema = PasteSchema()
         return {
-            "success": True,
-            "paste": schema.dump(paste)
+            'success': True,
+            'paste': schema.dump(paste)
         }
+
+
+class ActionError(Exception):
+    def __init__(self, error_type, status_code, message):
+        self.error_type = error_type
+        self.status_code = status_code
+        self.message = message
+
+class InvalidRequestError(ActionError):
+    def __init__(self, message, param):
+        super().__init__('invalid_request_error', 400, message)
+        self.param = param
+
+
+@name_space.errorhandler(InvalidRequestError)
+def handle_InvalidRequestError(error):
+    return {
+        'success': False,
+        'status': error.status_code,
+        'type': error.error_type,
+        'message': error.message,
+        'param': error.param
+    }, error.status_code
+
+handle_bad_request_400 = {
+    'success': False,
+    'status': 400,
+    'message': 'The browser (or proxy) sent a request that this server could \
+not understand.'
+}
+
+handle_not_found_404 = {
+    'success': False,
+    'status': 404,
+    'message': 'resource not found'
+}
